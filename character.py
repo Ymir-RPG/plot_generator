bc116bc3a5efa445056ec786d97fdf6630d538b2
Sage Berg <sberg@uoregon.edu>
2015-04-29 19:27:17 -0700

    added befriending

--- character.py
+++ character.py
@@ -28,11 +28,15 @@ class Character(object):
         return choice(male_first_list)
 
     def make_decision(self):
-        roll = randint(0, 1) 
-        if roll:
+        roll = randint(0, 2) 
+        if roll == 0:
             self.spawn()
-        else:
+        elif roll == 1:
             self.die()
+        else:
+            friend = choice(list(self.space)) #O(n), find way to fix
+            if friend != self: #can't befriend yourself
+                self.befriend(friend)
 
     def spawn(self):
         child = Character(self.space)
@@ -47,4 +51,12 @@ class Character(object):
         Character.dead.add(self)
 
     def befriend(self, other):
-        pass
+        if other in self.relations:
+            self.relations[other] += 1
+        else:
+            self.relations[other] = 1
+        if self in other.relations:
+            other.relations[self] += 1
+        else:
+            other.relations[self] = 1
+        self.action_log.append(self.create_entry("befriended " + other.name))
